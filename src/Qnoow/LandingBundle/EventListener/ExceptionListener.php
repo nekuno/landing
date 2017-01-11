19b85424965dc73aed64a5c1791382a3d0eae757
msalsas <manolez@gmail.com>
2017-01-11 15:09:23 +0100

    Add locale and exception listeners

---
+++ src/Qnoow/LandingBundle/EventListener/ExceptionListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Qnoow\LandingBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+
+class ExceptionListener
+{
+    protected $templating;
+    protected $kernel;
+
+    public function __construct(EngineInterface $templating, $kernel)
+    {
+        $this->templating = $templating;
+        $this->kernel = $kernel;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if ('prod' == $this->kernel->getEnvironment()) {
+            $exception = $event->getException();
+
+            $response = new Response();
+
+            $response->setContent(
+                $this->templating->render(
+                    'QnoowLandingBundle:Exception:exception.html.twig',
+                    array('exception' => $exception)
+                )
+            );
+
+            if ($exception instanceof HttpExceptionInterface) {
+                $response->setStatusCode($exception->getStatusCode());
+                $response->headers->replace($exception->getHeaders());
+            } else {
+                var_dump($exception);
+                exit;
+                $response->setStatusCode(500);
+            }
+
+            $event->setResponse($response);
+        }
+    }
+}
---
+++ src/Qnoow/LandingBundle/EventListener/LocaleListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Qnoow\LandingBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleListener implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale)
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        );
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+
+        $request = $event->getRequest();
+        $request->setDefaultLocale($this->defaultLocale);
+
+        $locale = $request->attributes->get('_locale');
+
+        if (!$locale && $request->query->has('_locale')) {
+            $locale = $request->query->get('_locale');
+        }
+
+        if (!$locale && !$request->hasPreviousSession() && $request->getPreferredLanguage()) {
+
+            $locale = $request->getPreferredLanguage();
+
+        } elseif (!$locale && $request->getSession()->has('_locale')) {
+
+            $locale = $request->getSession()->get('_locale');
+        }
+
+        if (!$locale) {
+            $locale = $this->defaultLocale;
+        }
+
+        if (strpos($locale, 'es_') !== false) {
+            $locale = 'es';
+        } elseif (strpos($locale, 'en_') !== false) {
+            $locale = 'en';
+        }
+
+        $request->setLocale($locale);
+        $request->getSession()->set('_locale', $locale);
+
+    }
+}
